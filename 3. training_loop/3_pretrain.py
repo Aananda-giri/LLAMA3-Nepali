391eec697a45a5e083b2230ce4bf90d656eba24b
aananda-giri <aanandaprashadgiri@gmail.com>
2025-01-21 11:30:19 +0545

    monor debug

--- 3. training_loop/3_pretrain.py
+++ 3. training_loop/3_pretrain.py
@@ -229,7 +229,7 @@ def train_model(model, train_loader, val_loader, optimizer, device,
                     pushed_to_hub_once = True
                     
             # Save at the end of each epoch
-            delete_checkpoints_except_n_highest_steps(n=1)  # modified. to delete the previous steps checkpoint
+            delete_checkpoints_except_n_highest_steps(folder_path=args.output_dir, n=1)  # modified. to delete the previous steps checkpoint
             new_epochs = global_step % len_train_loader if global_step > len_train_loader else 0
             save_file_path = os.path.join(output_dir, f"model_pg_epoch_{new_epochs}.pth")
             torch.save({
